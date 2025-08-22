880c2733a800e573dd1f218a44ca5fd1e85b3d46
davidmoi2135 <caocaonguyentrinh210305@gmail.com>
2025-08-22 10:40:51 +0700

    shareLink

--- chatapp-backend/src/main/java/com/example/chatapp/model/ChatMessage.java
+++ chatapp-backend/src/main/java/com/example/chatapp/model/ChatMessage.java
@@ -1,17 +1,4 @@
 package com.example.chatapp.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ChatMessage {
-    private String sender;
-    private String content;
-    private String type;
-    private String roomId;
 }
